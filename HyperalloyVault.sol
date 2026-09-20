71889ae7c68d6b5bfcf33caff5a537dd0539d062
promiseazebiri7-cloud <promiseazebiri7@gmail.com>
2026-09-20 23:03:26 +0100

    Update HyperalloyVault.sol

--- HyperalloyVault.sol
+++ HyperalloyVault.sol
@@ -5,59 +5,71 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+interface IValuedToken is IERC20 {
+    function assetValueUSD() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+}
+
 contract HyperalloyVault is ERC20, Ownable {
-    IERC20 public realEstateToken;
-        IERC20 public goldToken;
+    IValuedToken public realEstateToken;
+    IValuedToken public goldToken;
 
-            // Basket token is minted 1:1 with the total USD value deposited
-                constructor(address _realEstateToken, address _goldToken)
-                        ERC20("Hyperalloy Basket Token", "HALLOY")
-                                Ownable(msg.sender)
-                                    {
-                                            realEstateToken = IERC20(_realEstateToken);
-                                                    goldToken = IERC20(_goldToken);
-                                                        }
+    constructor(address _realEstateToken, address _goldToken)
+        ERC20("Hyperalloy Basket Token", "HALLOY")
+        Ownable(msg.sender)
+    {
+        realEstateToken = IValuedToken(_realEstateToken);
+        goldToken = IValuedToken(_goldToken);
+    }
 
-                                                            // Deposit both assets, receive basket tokens representing your share
-                                                                function deposit(uint256 realEstateAmount, uint256 goldAmount) external {
-                                                                        require(realEstateAmount > 0 || goldAmount > 0, "Must deposit something");
+    function _valueOf(IValuedToken token, uint256 amount) internal view returns (uint256) {
+        uint256 supply = token.totalSupply();
+        if (supply == 0) {
+            return 0;
+        }
+        return (amount * token.assetValueUSD()) / supply;
+    }
 
-                                                                                if (realEstateAmount > 0) {
-                                                                                            realEstateToken.transferFrom(msg.sender, address(this), realEstateAmount);
-                                                                                                    }
-                                                                                                            if (goldAmount > 0) {
-                                                                                                                        goldToken.transferFrom(msg.sender, address(this), goldAmount);
-                                                                                                                                }
+    function deposit(uint256 realEstateAmount, uint256 goldAmount) external {
+        require(realEstateAmount > 0 || goldAmount > 0, "Must deposit something");
 
-                                                                                                                                        // For this simple prototype: mint basket tokens equal to total deposited amount
-                                                                                                                                                uint256 basketAmount = realEstateAmount + goldAmount;
-                                                                                                                                                        _mint(msg.sender, basketAmount);
-                                                                                                                                                            }
+        if (realEstateAmount > 0) {
+            realEstateToken.transferFrom(msg.sender, address(this), realEstateAmount);
+        }
 
-                                                                                                                                                                // Withdraw your share — burns basket tokens, returns underlying assets proportionally
-                                                                                                                                                                    function withdraw(uint256 basketAmount) external {
-                                                                                                                                                                            require(balanceOf(msg.sender) >= basketAmount, "Insufficient basket tokens");
+        if (goldAmount > 0) {
+            goldToken.transferFrom(msg.sender, address(this), goldAmount);
+        }
 
-                                                                                                                                                                                    uint256 totalSupplyBefore = totalSupply();
-                                                                                                                                                                                            uint256 realEstateBalance = realEstateToken.balanceOf(address(this));
-                                                                                                                                                                                                    uint256 goldBalance = goldToken.balanceOf(address(this));
+        uint256 depositValue = _valueOf(realEstateToken, realEstateAmount) + _valueOf(goldToken, goldAmount);
+        require(depositValue > 0, "Deposit value must be greater than zero");
 
-                                                                                                                                                                                                            uint256 realEstateShare = (realEstateBalance * basketAmount) / totalSupplyBefore;
-                                                                                                                                                                                                                    uint256 goldShare = (goldBalance * basketAmount) / totalSupplyBefore;
+        _mint(msg.sender, depositValue);
+    }
 
-                                                                                                                                                                                                                            _burn(msg.sender, basketAmount);
+    function withdraw(uint256 basketAmount) external {
+        require(balanceOf(msg.sender) >= basketAmount, "Insufficient basket tokens");
 
-                                                                                                                                                                                                                                    if (realEstateShare > 0) {
-                                                                                                                                                                                                                                                realEstateToken.transfer(msg.sender, realEstateShare);
-                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                if (goldShare > 0) {
-                                                                                                                                                                                                                                                                            goldToken.transfer(msg.sender, goldShare);
-                                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                                        }
+        uint256 totalSupplyBefore = totalSupply();
+        uint256 realEstateBalance = realEstateToken.balanceOf(address(this));
+        uint256 goldBalance = goldToken.balanceOf(address(this));
 
-                                                                                                                                                                                                                                                                                            // View function: see the vault's total holdings of each asset
-                                                                                                                                                                                                                                                                                                function getVaultHoldings() external view returns (uint256 realEstateHeld, uint256 goldHeld) {
-                                                                                                                                                                                                                                                                                                        realEstateHeld = realEstateToken.balanceOf(address(this));
-                                                                                                                                                                                                                                                                                                                goldHeld = goldToken.balanceOf(address(this));
-                                                                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                                                                    }
+        uint256 realEstateShare = (realEstateBalance * basketAmount) / totalSupplyBefore;
+        uint256 goldShare = (goldBalance * basketAmount) / totalSupplyBefore;
+
+        _burn(msg.sender, basketAmount);
+
+        if (realEstateShare > 0) {
+            realEstateToken.transfer(msg.sender, realEstateShare);
+        }
+
+        if (goldShare > 0) {
+            goldToken.transfer(msg.sender, goldShare);
+        }
+    }
+
+    function getVaultHoldings() external view returns (uint256 realEstateHeld, uint256 goldHeld) {
+        realEstateHeld = realEstateToken.balanceOf(address(this));
+        goldHeld = goldToken.balanceOf(address(this));
+    }
+}
